d193f4f51cc8aa9b78c1b5805a862f765395168e
Paweł Boczarski <pawel.boczarski@gmail.com>
2015-01-02 14:34:12 +0100

    README added

---
+++ README.md
@@ -0,0 +1,1 @@
+Bash implementation of Diana Cryptosystem described at http://programmingpraxis.com
--- RUN_ME_FIRST_TIME.sh
+++ RUN_ME_FIRST_TIME.sh
@@ -4,3 +4,6 @@ echo "Making cipher book..."
 ./make_cipher_book.sh > cipher_book 
 echo "Generating one time pad..."
 ./make_one_time_pad.sh 1000 > one_time_pad
+
+echo "Run './do_cipher.sh --help' to see how to cipher and decipher"
+
